1ae75aa576eb516eac459209fb78b75a03d08bf4
Ernesto Rojas <ernesto20145@gmail.com>
2024-10-18 19:27:02 -0300

    Initial commit

---
+++ src/users/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.schema';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() user: User): Promise<User> {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<User> {
+    return this.userService.delete(id);
+  }
+}
